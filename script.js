d1c694f431da7d2cc1e2d51f5325b87743984c01
RRachelvd <156345931+RRachelvd@users.noreply.github.com>
2024-02-28 14:27:47 +0100

    Search engine added

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
+function searchCityWeather(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
 
-
+let searchInput = document.querySelector("#search-form");
+searchInput.addEventListener("submit", searchCityWeather);
