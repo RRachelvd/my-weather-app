69aec9649703f98d306a7e168122b1769128e32d
RRachelvd <156345931+RRachelvd@users.noreply.github.com>
2024-02-28 15:51:14 +0100

    Display info from API

--- script.js
+++ script.js
@@ -1,10 +1,47 @@
 function updateWeather(response) {
-  console.log(response.data.temperature.current);
   let currentTemperature = response.data.temperature.current;
+  let currentDescription = response.data.condition.description;
+  let currentWind = response.data.wind.speed;
+  let currentHumidity = response.data.temperature.humidity;
   let temperatureElement = document.querySelector("#weather-app-temperature");
-  temperatureElement.innerHTML = Math.round(currentTemperature);
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#weather-description");
+  let windElement = document.querySelector("#wind");
+  let humidityElement = document.querySelector("#humidity");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
+  windElement.innerHTML = `${Math.round(currentWind)} km/h`;
+  descriptionElement.innerHTML = currentDescription;
+  humidityElement.innerHTML = `${currentHumidity}%`;
+  temperatureElement.innerHTML = Math.round(currentTemperature);
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let timeHours = date.getHours();
+  let timeMinutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (timeHours < 10) {
+    timeHours = `0${timeHours}`;
+  }
+
+  if (timeMinutes < 10) {
+    timeMinutes = `0${timeMinutes}`;
+  }
+
+  return `${day} ${timeHours}:${timeMinutes}, `;
 }
 
 function searchCity(city) {
