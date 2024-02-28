7fd5e507afc72d68661028d38af8b61c52581a8b
RRachelvd <156345931+RRachelvd@users.noreply.github.com>
2024-02-28 14:58:10 +0100

    Added API

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-function searchCityWeather(event) {
+function updateWeather(response) {
+  console.log(response.data.temperature.current);
+  let currentTemperature = response.data.temperature.current;
+  let temperatureElement = document.querySelector("#weather-app-temperature");
+  temperatureElement.innerHTML = Math.round(currentTemperature);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "0c98c0be68f4tba31fe26f898obb603d";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
+function searchSumbit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchInput = document.querySelector("#search-form");
-searchInput.addEventListener("submit", searchCityWeather);
+searchInput.addEventListener("submit", searchSumbit);
+
+searchCity("Leeuwarden");
